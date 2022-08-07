4224352926e59da43c3901ad2f799536b027269e
Amit Mishra <77039681+amitmishrra@users.noreply.github.com>
2022-08-07 13:07:35 -0700

    Add files via upload

---
+++ RemoveDuplicates.js
@@ -0,0 +1,22 @@
+let array = [1,1,1,1];
+let newArray = [];
+let bool = false;
+count = 0;
+
+const removeDuplicate = (array)=>{
+  for(i=0; i<array.length; i++){
+    for(j=i; j<array.length; j++){
+    if(array[i]==array[j] && i!=j){
+      count = count + 1;
+      bool = true;
+      break
+    }else{
+      bool = false
+    }
+    }
+    if(!bool){newArray.push(array[i])}
+  }
+  return newArray;
+}
+
+console.log(removeDuplicate(array), count);
